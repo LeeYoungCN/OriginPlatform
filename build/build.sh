7f2e05bbe9e98deafbe5c0cd81c93940222c54ba
LiYang <1175529741@qq.com>
2022-04-28 21:04:26 +0800

    linux

--- build/build.sh
+++ build/build.sh
@@ -1,20 +1,28 @@
 #!/bin/bash
 cd ..
 
+cache_path=buildcache
+output_path=output
+release_path=release
+component_name=PublicFunctionTest
+test_exe=${RunTest}
+
 if [ ! -d buildcache ]; then
     mkdir buildcache
 fi
+
+rm -rf ${cache_path}
+rm -rf ${output_path}
+rm -rf ${release_path}
+
 os=$(uname -s)
 os=${os%_*}
-    
-pushd ./buildcache
-rm -rf *
 if [[ ${os} == "MINGW"* ]]; then
-    cmake -S .. -G "MinGW Makefiles"
+    cmake -S . -B ${cache_path}  -G "MinGW Makefiles"
 else
-    cmake -S ..
+    cmake -S . -B ${cache_path}
 fi
-make -j8
-make install
-popd
-./output/TestPlatform.exe
+cmake --build ${cache_path} -j8
+cmake --install ${cache_path} --component ${component_name}
+
+./${output_path}/${test_exe}*
