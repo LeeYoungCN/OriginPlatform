dceac283ee30e0043dd4aa2e79230dfc46ec5744
LiYang <1175529741@qq.com>
2022-09-11 10:43:20 +0800

    darwin

--- cmake/Windows/os_variable_def.cmake
+++ cmake/Windows/os_variable_def.cmake
@@ -1,4 +1,5 @@
 set(MINGW_PATH "D:/Program Files/mingw64")
 set(CMAKE_C_COMPLIER ${MINGW_PATH}/bin/gcc.exe)
 set(CMAKE_CXX_COMPLIER ${MINGW_PATH}/bin/g++.exe)
-set(DANYMIC_LIBRARY_SUFFIX dll)
+set(DANYMIC_LIBRARY_SUFFIX .dll)
+set(STATIC_LIBRARY_SUFFIX .a)
